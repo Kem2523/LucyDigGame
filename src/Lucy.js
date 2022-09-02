137c9dfd1b2bfc92c3eb60e717f8fa55a469c931
Kem2523 <krystalemorris@gamil.com>
2022-09-01 21:14:34 -0500

    Tile Map done, movement partially complete

---
+++ src/Lucy.js
@@ -0,0 +1,104 @@
+import MovingDirection from "./MovingDirection.js";
+export default class Lucy {
+  constructor(x, y, tileSize, velocity, tileMap) {
+    this.x = x;
+    this.y = y;
+    this.tileSize = tileSize;
+    this.velocity = velocity;
+    this.tileMap = tileMap;
+
+    this.currentMovingDirection = null;
+    this.requestedMovingDirection = null;
+
+    document.addEventListener("keydown", this.#keydown);
+    this.#loadLucyImages();
+  }
+  draw(ctx) {
+    this.#move();
+    ctx.drawImage(
+      this.lucyImages[this.lucyImageIndex],
+      this.x,
+      this.y,
+      this.tileSize,
+      this.tileSize
+    );
+  }
+
+  #loadLucyImages() {
+    const lucyImage1 = new Image();
+    lucyImage1.src = "../images/dog (1).png";
+
+    const lucyImage2 = new Image();
+    lucyImage2.src = "../images/dog1.png";
+
+    const lucyImage3 = new Image();
+    lucyImage3.src = "../images/dog3.png";
+
+    const lucyImage4 = new Image();
+    lucyImage4.src = "../images/dog1.png";
+
+    this.lucyImages = [lucyImage1, lucyImage2, lucyImage3, lucyImage4];
+
+    this.lucyImageIndex = 2;
+  }
+  #keydown = (event) => {
+    //up
+    if (event.keyCode == 38) {
+      if (this.currentMovingDirection == MovingDirection.down)
+        this.currentMovingDirection = MovingDirection.up;
+      this.requestedMovingDirection = MovingDirection.up;
+    }
+    //down
+    if (event.keyCode == 40) {
+      if (this.currentMovingDirection == MovingDirection.up)
+        this.currentMovingDirection = MovingDirection.down;
+      this.requestedMovingDirection = MovingDirection.down;
+    }
+    //left
+    if (event.keyCode == 37) {
+      if (this.currentMovingDirection == MovingDirection.right)
+        this.currentMovingDirection = MovingDirection.left;
+      this.requestedMovingDirection = MovingDirection.left;
+    }
+    //right
+    if (event.keyCode == 39) {
+      if (this.currentMovingDirection == MovingDirection.left)
+        this.currentMovingDirection = MovingDirection.right;
+      this.requestedMovingDirection = MovingDirection.right;
+    }
+  };
+  #move() {
+    if (this.currentMovingDirection !== this.requestedMovingDirection) {
+      if (
+        Number.isInteger(this.x / this.tileSize) &&
+        Number.isInteger(this.y / this.tileSize)
+      ) {
+        if (
+          !this.tileMap.didCollideWithEnvironment(
+            this.x,
+            this.y,
+            this.requestedMovingDirection
+          )
+        )
+          this.currentMovingDirection = this.requestedMovingDirection;
+      }
+    }
+
+if(this.tileMap.didCollideWithEnvironment(this.x, this.y, this.currentMovingDirection))
+
+    switch (this.currentMovingDirection) {
+      case MovingDirection.up:
+        this.y -= this.velocity;
+        break;
+      case MovingDirection.down:
+        this.y += this.velocity;
+        break;
+      case MovingDirection.left:
+        this.x -= this.velocity;
+        break;
+      case MovingDirection.right:
+        this.x += this.velocity;
+        break;
+    }
+  }
+}
---
+++ src/TileMap.js
@@ -0,0 +1,144 @@
+import Lucy from "./Lucy.js";
+import MovingDirection from "./MovingDirection.js";
+export default class TileMap {
+  constructor(tileSize) {
+    this.tileSize = tileSize;
+
+    this.dirt = new Image();
+    this.dirt.src = "../images/dirt2.png";
+
+    this.dirtGrass = new Image();
+    this.dirtGrass.src = "../images/dirtGrass.png";
+
+    this.bone = new Image();
+    this.bone.src = "../images/bone.png";
+  }
+
+  map = [
+    [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 4, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  ];
+
+  draw(ctx) {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        let tile = this.map[row][column];
+        if (tile === 2) {
+          this.#drawDirt(ctx, column, row, this.tileSize);
+        } else if (tile === 1) {
+          this.#drawDirtGrass(ctx, column, row, this.tileSize);
+        } 
+       else if (tile === 4) {
+          this.#drawBone(ctx, column, row, this.tileSize);
+        }
+      }
+    }
+  }
+
+  #drawDirt(ctx, column, row, size) {
+    ctx.drawImage(
+      this.dirt,
+      column * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
+  }
+  #drawDirtGrass(ctx, column, row, size) {
+    ctx.drawImage(
+      this.dirtGrass,
+      column * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
+  }
+  
+  #drawBone(ctx, column, row, size) {
+    ctx.drawImage(
+      this.bone,
+      column * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
+  }
+
+  getLucy(velocity) {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        let tile = this.map[row][column];
+        if (tile === 0) {
+            this.map[row][column]=  2
+          return new Lucy(
+            column * this.tileSize,
+            row * this.tileSize,
+            this.tileSize,
+            velocity,
+            this
+          );
+        }
+      }
+    }
+  }
+  setCanvasSize(canvas) {
+    canvas.width = this.map[0].length * this.tileSize;
+    canvas.height = this.map.length * this.tileSize;
+  }
+
+  didCollideWithEnvironment(x, y, direction) {
+    if(direction = null){
+        return;
+    }
+    
+    if (
+      Number.isInteger(x / this.tileSize) &&
+      Number.isInteger(y / this.tileSize)
+    ) {
+      let column = 0;
+      let row = 0;
+      let nextColumn = 0;
+      let nextRow = 0;
+
+      switch (direction) {
+        case MovingDirection.right:
+          nextColumn = x + this.tileSize;
+          column = nextColumn / this.tileSize;
+          row = y / this.tileSize;
+          break;
+          case MovingDirection.left:
+          nextColumn = x - this.tileSize;
+          column = nextColumn / this.tileSize;
+          row = y / this.tileSize;
+          break;
+          case MovingDirection.up:
+          nextRow = y - this.tileSize;
+          row = nextRow / this.tileSize;
+          column = x / this.tileSize;
+          break;
+          case MovingDirection.down:
+          nextRow = y + this.tileSize;
+          row = nextRow / this.tileSize;
+          column = x / this.tileSize;
+          break;
+      }
+          const tile = this.map[row][column];
+          if (tile === 1){
+              return true;
+          }
+      }
+      return false;
+    }
+  }
+
